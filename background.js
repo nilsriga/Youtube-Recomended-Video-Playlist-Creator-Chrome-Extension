81e01fbdd0ae90740c7300fc04bff3d9c9762d33
Users <Usey>
2019-05-17 18:03:41 +0300

    Added alert

--- background.js
+++ background.js
@@ -84,6 +84,25 @@ chrome.browserAction.onClicked.addListener(function () {
         // this authorizes requests
         chrome.identity.getAuthToken({ interactive: true }, function (token) {
 
+
+            chrome.webRequest.onHeadersReceived.addListener(
+                function(info) {
+                    var headers = info.responseHeaders;
+                    for (var i=headers.length-1; i>=0; --i) {
+                        var header = headers[i].name.toLowerCase();
+                        if (header == 'x-frame-options' || header == 'frame-options') {
+                            headers.splice(i, 1); // Remove header
+                        }
+                    }
+                    return {responseHeaders: headers};
+                },
+                {
+                    urls: [ '*://*/*' ], // Pattern to match all http(s) pages
+                    types: [ 'sub_frame' ]
+                },
+                ['blocking', 'responseHeaders']
+            );
+
             // This is a GET reques to get "TV" playlist's id
             let gettingTvPlaylistId = {
                 method: 'GET',
